c14c653c029ab18251cc4686576ddd87407f7888
BenjaminOrtega <eduardobenjamin.cortes@outlook.com>
2021-04-27 22:34:21 -0500

    first commit

---
+++ src/main/java/com/app/personas/GestionPersonasBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.app.personas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPersonasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPersonasBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/personas/GestionPersonasBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.personas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPersonasBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
